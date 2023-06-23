1354c42721e3b6bf9e2d403ab1a79b3bb04ac872
tedhangxanh <tedhangxanh@pm.me>
2023-06-23 02:32:37 -0500

    Complete basic of converter

--- Converter.py
+++ Converter.py
@@ -1,35 +1,12 @@
 import os
-import logging
-import logging.handlers
-import subprocess as sp
-import json
-from multiprocessing.pool import ThreadPool
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 class AudioConverter:
-    def __init__(self, file_name):
+    def __init__(self, file_name: str):
         self.file_name = file_name
         self.FFMPEG_BIN = "ffmpeg"
         self.filepath, self.ext = os.path.splitext(self.file_name)
-
-    def convert_to_m4a(self):
-        """
-        Converts a input file to m4a
-
-        command: ffmpeg -i input.wav -c:a aac -b:a 160k output.m4a
-        ffmpeg -i input.wav -c:a aac -b:a 160k output.m4a
-        """
-        codec = "aac"
-        m4a_filename = self.filepath + ".m4a"
-        command = [self.FFMPEG_BIN,
-                   "-n",
-                   "-i", self.file_name,
-                   "-acodec", codec,
-                   "-ab", "128k",
-                   m4a_filename
-                   ]
-
-        return command
 
     def convert_to_mp3(self):
         """
@@ -44,7 +21,6 @@ class AudioConverter:
         mp3_filename = self.filepath + ".mp3"
 
         command = [self.FFMPEG_BIN,
-                   "-n",
                    "-i", self.file_name,
                    "-acodec", codec,
                    "-ab", "128k",
@@ -75,3 +51,10 @@ class AudioConverter:
                    ]
 
         return command
+
+    def convert_to_audio(self, ext: str):
+        ext = '.' + ext.strip('.')
+        new_filename = self.filepath + ext
+        clip = VideoFileClip(self.file_name)
+        audio = clip.audio
+        audio.write_audiofile(new_filename)
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from Converter import AudioConverter
-from pathlib import Path as p
-import moviepy.editor as mp
-from pydub import AudioSegment
 import subprocess as sp
 
-audio_file = './original audio.ogg'
+audio_file = './original audio.mp3'
 video_file = "./original video.mp4"
 output_ext = '.mp3'
 
 converter = AudioConverter(audio_file)
-sp.run(converter.convert_to_mp3())
+# sp.run(converter.convert_to_mp3())
+# sp.run(converter.convert_to_ogg())
+# sp.run(converter.convert_to_audio('wav'))
