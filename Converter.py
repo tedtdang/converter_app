94a4d4e1645ee7160525df8999ed23b005cfdceb
tedhangxanh <tedhangxanh@pm.me>
2023-06-23 03:00:56 -0500

    Combine all methods in AudioConverter class into one for all

--- Converter.py
+++ Converter.py
@@ -1,60 +1,35 @@
 import os
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from pathlib import Path as p
+
+import ffmpeg
 
 
 class AudioConverter:
+    video_extensions = {'.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm'}
+    audio_extensions = {'.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a'}
+
     def __init__(self, file_name: str):
         self.file_name = file_name
         self.FFMPEG_BIN = "ffmpeg"
         self.filepath, self.ext = os.path.splitext(self.file_name)
 
-    def convert_to_mp3(self):
+    def convert_to_audio(self, ext: str):
         """
         Converts a input file to mp3
 
         command: ffmpeg -n -i input.m4a -acodec libmp3lame -ab 128k output.mp3
         """
-        if self.ext == ".mp3":
-            return
-
-        codec = "libmp3lame"
-        mp3_filename = self.filepath + ".mp3"
-
-        command = [self.FFMPEG_BIN,
-                   "-i", self.file_name,
-                   "-acodec", codec,
-                   "-ab", "128k",
-                   mp3_filename
-                   ]
-        return command
-
-    def convert_to_ogg(self):
-        """
-        Converts a input file to ogg
-
-        command: ffmpeg -n -i input.m4a -acodec libvorbis -aq 60 -vn -ac 2 output.ogg
-        """
-        if self.ext == ".ogg":
-            return
-
-        codec = "libvorbis"
-        ogg_filename = self.filepath + ".ogg"
-
-        command = [self.FFMPEG_BIN,
-                   "-n",
-                   "-i", self.file_name,
-                   "-acodec", codec,
-                   "-aq", "60",
-                   "-vn",
-                   "-ac", "2",
-                   ogg_filename
-                   ]
-
-        return command
-
-    def convert_to_audio(self, ext: str):
         ext = '.' + ext.strip('.')
-        new_filename = self.filepath + ext
-        clip = VideoFileClip(self.file_name)
-        audio = clip.audio
-        audio.write_audiofile(new_filename)
+        if self.ext == ext:
+            return
+        output_file = str(p(self.file_name).with_suffix('').with_suffix(ext))
+        input_stream = ffmpeg.input(self.file_name)
+        file_ext = p(self.file_name).suffix
+        if file_ext in AudioConverter.video_extensions:
+            output_stream = ffmpeg.output(input_stream.audio, output_file)
+        else:
+            output_stream = ffmpeg.output(input_stream, output_file)
+        # Set the codec and format options
+        output_stream = output_stream.global_args('-hwaccel', 'cuvid')
+        # Run the conversion
+        ffmpeg.run(output_stream)
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 from Converter import AudioConverter
-import subprocess as sp
 
 audio_file = './original audio.mp3'
 video_file = "./original video.mp4"
-output_ext = '.mp3'
 
 converter = AudioConverter(audio_file)
-# sp.run(converter.convert_to_mp3())
-# sp.run(converter.convert_to_ogg())
-# sp.run(converter.convert_to_audio('wav'))
+converter.convert_to_audio(ext='mp3')
